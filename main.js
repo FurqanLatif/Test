597cdeb67cb2fa0389782558eba7cb8e4cedc214
FurqanLatif <32981578+FurqanLatif@users.noreply.github.com>
2018-04-13 19:07:46 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function change_slider () {
+  document.getElementById('change_slider_one').src = 'images/1.jpg'
+  document.getElementById('change_slider_three').src = 'images/3.jpg'
+}
+function change_slider_t () {
+  document.getElementById('change_slider_one').src = 'images/2.jpg'
+  document.getElementById('change_slider_one').style.border = '9px solid black'
+}
